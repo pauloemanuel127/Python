680a2fb29848b50eff77f8f278c9b7d06a6afa1d
Paulo Gois <pauloemanuel127@gmail.com>
2026-04-15 19:10:48 -0300

    fixing a error on the code

--- Beecrowd/AD-HOC/BC_1087.py
+++ Beecrowd/AD-HOC/BC_1087.py
@@ -9,7 +9,10 @@ while True:
     y2 = int(moves[3])
     X = (x1, y1)
     Y = (x2, y2)
-    if 1 < x1 < 8 and 1 < x2 < 8 and 1 < y1 < 8 and 1 < y2 < 8:
+    #In this part of the code, the `if` statement checks if the input is true with respect to the chessboard,
+    #or, if the input is responsible for the interruption condition; otherwise, it defines that the input is not true with respect to the chessboard.
+    #Then, another `if` statement checks how many moves the queen will make.
+    if 1 <= x1 <= 8 and 1 <= x2 <= 8 and 1 <= y1 <= 8 and 1 <= y2 <= 8:
         if X == Y:
             print('0')
         elif x1 == x2 or y1 == y2 or abs(x1 - x2) == abs(y1 - y2):
@@ -19,4 +22,5 @@ while True:
     elif x1 == 0 and x2 == 0 and y1 == 0 and y2 == 0:
         break
     else:
-        break
+        print('The input values do not correspond to the format of a chessboard')
+        
