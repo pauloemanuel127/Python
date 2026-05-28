a9690c2ba0378fdb39c85cf9f74826828bd3610d
Paulo Gois <pauloemanuel127@gmail.com>
2026-05-28 18:56:11 -0300

    Fixing some indentations

--- tests_and_random_things/pensamento_computacional/atv1.py
+++ tests_and_random_things/pensamento_computacional/atv1.py
@@ -4,12 +4,17 @@ valor =  int(input())
 forma = input()
 
 if forma == "V":
+
     valor = int(valor - ((valor * 5) / 100))
+
     print(f"Valor a pagar: {valor}")
 
 elif forma == "P":
+
     taxa = int((valor * 8) / 100)
     parcela = int((valor + taxa) / 3)
+
     print(f"Valor a pagar: {valor + taxa}")
+
     for i in range(1, 4):
         print(f"Parcela {i}: {parcela}")
