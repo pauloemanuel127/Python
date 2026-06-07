35e8f00df747af60a5ff8cc522341fef2e9ae022
Paulo Gois <pauloemanuel127@gmail.com>
2026-06-07 18:00:14 -0300

    Docs: Fix language erros

--- tests_and_random_things/binary_search.py
+++ tests_and_random_things/binary_search.py
@@ -1,19 +1,26 @@
-#This program has a binary search function, an algorithm with the complexity equals O(logn)
-#Realizes two test cases to test the algorithm
+#This program has a binary search function, an algorithm with a complexity of O(log(n))
 
-def binary_search(list, iten):
-    """This function search for a specific element in a ordered list,
-    the operation only occurs in a ordered list"""
+def binary_search(array, item):
+    """
+    This function receives a sorted array and an item to search for.
+    Returns the index of the value, or None if it isn't in the array.
+    """
+
     low = 0
-    high = len(list) - 1
+    high = len(array) - 1
 
     while low <= high:
+
         mid = int((low + high) / 2)
-        guess = list[mid]
-        if guess == iten:
+        guess = array[mid]
+
+        if guess == item:
             return mid
-        if guess > iten:
+        
+        if guess > item:
             high = mid - 1
+
         else:
             low = mid + 1
+
     return None
--- tests_and_random_things/linked_list.py
+++ tests_and_random_things/linked_list.py
@@ -1,47 +1,47 @@
-# Trying to make a linked list algorith, 
-# The linked list have O(1) complexity for insertion and delete, but O(n) for searching.
-# Meanwhile Arrays have O(n) for insertion and delete, but O(1) for search.
+# Trying to make a linked list algorithm, 
+# The linked list has O(1) complexity for insertion and deletion, but O(n) for searching.
+# Meanwhile Arrays have O(n) for insertion and deletion, but O(1) for searching.
 
 def create_node(value):
     """
-    This function creates the node that will always be the last one in the linked list.
+    This function creates a node that will initially be the last one in the linked list.
     """
 
-    # Nessa função ele recebe o valor desejado e cria o nó dele na lista encadeada, esse valor será sempre adicionado no final da lista
+    # Nessa função ele recebe o valor desejado e cria o nó dele na lista encadeada, esse valor será sempre adicionado no final da lista.
     return {'value': value, 'next': None}
 
 def append(head, value):
     """
-    This function adds a new element who will be the last node in the list until a new one is appended.
+    This function adds a new element that will be the last node in the list until a new one is appended.
     """
 
     # Nessa função ele recebe a lista entitulada como head e o valor que será inserido,
-    # Logo no começo ele usa da função create_node para criar o novo nó na lista
+    # Logo no começo ele usa da função create_node para criar o novo nó na lista.
     new_node = create_node(value)
 
-    # Aqui ele verifica se a lista está vazia, caso estiver ele retorna o novo nó como elemento
+    # Aqui ele verifica se a lista está vazia, caso estiver ele retorna o novo nó como elemento.
     if head is None:
         return new_node
     
-    # Seguindo o caminho ele coloca que o ponto atual é o primeiro elemento da lista que vai sempre está como head
+    # Seguindo o caminho ele coloca que o ponto atual é o primeiro elemento da lista que vai sempre está como head.
     current = head
 
     # Nesse loop while, ele testa que enquanto o proximo elemento da lista não tiver chavemento None, ele irá passar para o proximo
-    # Já que o elemento adicionado será sempre no final
+    # Já que o elemento adicionado será sempre no final.
     while current['next'] is not None:
         current = current['next']
 
-    # Após isso ele coloca que o ultimo elemento será no novo nó e retorna a lista alterada
+    # Após isso ele coloca que o ultimo elemento será no novo nó e retorna a lista alterada.
     current['next'] = new_node
     return head
 
 def remove(head, value):
     """
-    This function removes the chosen element in the linked list.
+    This function removes the chosen element from the linked list.
     """
 
     # Aqui ele primeiro verifica se o valor desejado a ser removido é o primeiro elemento, 
-    # Caso for ele irá remover o elemento e a função acaba
+    # Caso for ele irá remover o elemento e a função acaba.
     if head['value'] == value:
         head = head['next']
         return head
@@ -50,7 +50,7 @@ def remove(head, value):
     # Caso o elemento estiver presente na lista ele irá remove-lo
     # Se não estiver ele retorna apenas a lista sem alterações
     # O loop while usa da mesma logica do append, 
-    # porém ele passa do elemento 2 para o elemento seguinte sempre procurando o valor desejado
+    # porém ele passa do elemento 2 para o elemento seguinte sempre procurando o valor desejado.
     else:
         current = head
 
@@ -66,11 +66,11 @@ def remove(head, value):
 
 def update(head, value, new_value):
     """
-    This function receives the list, the target element, and the new one.
-    Searches linearly if the target is on the list,
-    if it's on the list substitutes the value for the new one
-    if not print a message saying the element isn't on the list.
+    This function receives the list, the target element, and the new value.
+    It searches linearly for the target in the list; if found, it replaces the value with the new one.
+    If not, it prints a message saying the element isn't in the list.
     """
+    
     # Aqui ele coloca o ponteiro e verifica se é None, caso sejá ele não entra no loop,
     # caso não entramos no processo de busca linear até o elemento solicitado, que irá ser alterado
     # se o elemento desejado for encontrado ele irá ser substituido pelo novo valor
@@ -95,15 +95,15 @@ def update(head, value, new_value):
 
 def find(head, target):
     """
-    This function searches linearly if the target it on the list,
-    if it's on the list returns a message with its position,
-    if not returns a message saying it isn't present.
+    This function searches linearly to check if the target is in the list.
+    If it is, it returns a message with its position.
+    If not, it returns a message saying it isn't present.
     """
 
     # Aqui ele coloca o ponteiro apontando pra lista e verifica se a posição do proximo elemento é None, 
     # Se não for segue o funcionamento, ele verifica se o valor atual é o procurado, se for ele exibe a mensagem
     # Se não for ele aumenta a posição em mais 1 e avança pra o proximo elemento,
-    # Se o elemento não for encontrado ele informa que não está na lista
+    # Se o elemento não for encontrado ele informa que não está na lista.
     current = head
     posicao = 0
 
@@ -122,12 +122,12 @@ def find(head, target):
     
 def display(head):
     """
-    This function prints the elements on the linked list.
+    This function prints the elements of the linked list.
     """
 
     # Aqui ele pega todos os elementos da lista até o ultimo que é quando o valor de current['next'] for igual a None
     # Trasforma os elementos em uma string e armazena numa lista nativa do python,
-    # Depois exibe os elementos presentes nessa lista até indicar o final
+    # Depois exibe os elementos presentes nessa lista até indicar o final.
     current = head
     elements = []
     
